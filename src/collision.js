be69efeb3585965eb20fce4ef4687c2dfe94009b
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2023-01-26 13:54:28 +0100

    Create collision.js

--- src/collision.js
+++ src/collision.js
@@ -1,2 +1,22 @@
-var data =[ 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0]; 
+function collisionEnemy(player,enemy)
+{
+    if(player.position.y + player.height <=enemy.position.y && 
+        player.position.y + player.height + player.velocity.y >= enemy.position.y && 
+        player.position.x + player.width  >= enemy.position.x && 
+        player.position.x  <= platform.width + enemy.position.x )
+    {
+        return true;
+    }
+    else return false; 
+}
 
+function collisionPlatform(platform, player)
+{
+    if(player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y && 
+        player.position.x + player.width  >= platform.position.x +55 && 
+        player.position.x  <= platform.width + platform.position.x -190)
+        {
+        return true;   
+        }
+} 
