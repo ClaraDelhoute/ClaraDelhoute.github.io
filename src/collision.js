5398814cdad17cb709c808d0b3cd3c5e72edb014
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2023-01-27 18:06:30 +0100

    Update collision.js

--- src/collision.js
+++ src/collision.js
@@ -1,21 +1,30 @@
-function collisionEnemy(player,enemy)
+function collisionEnemyOnTop(player,enemy)
 {
     if(player.position.y + player.height <=enemy.position.y && 
         player.position.y + player.height + player.velocity.y >= enemy.position.y && 
-        player.position.x + player.width  >= enemy.position.x && 
-        player.position.x  <= platform.width + enemy.position.x )
+        player.position.x + player.width >= enemy.position.x +50 && 
+        player.position.x  <= platform.width + enemy.position.x-300)
     {
         return true;
     }
-    else return false; 
+}
+
+function collisionPlatformOnBottom(platform,player)
+{
+    if(player.position.y+player.height == platform.position.y &&
+        player.position.x+width<=platform.position.x+platform.width &&
+        player.position.x <= platform.position.x+platform.width)
+        {
+            return true; 
+        }
 }
 
 function collisionPlatform(platform, player)
 {
     if(player.position.y + player.height <= platform.position.y && 
         player.position.y + player.height + player.velocity.y >= platform.position.y && 
-        player.position.x + player.width  >= platform.position.x +55 && 
-        player.position.x  <= platform.width + platform.position.x -190)
+        player.position.x + player.width  >= platform.position.x +55  && 
+        player.position.x  <= platform.width + platform.position.x -platform.width/2)
         {
         return true;   
         }
