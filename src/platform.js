a3960383184bf2590e6ef4f75eb40340ce2d175e
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2023-01-27 18:07:09 +0100

    Update platform.js

--- src/platform.js
+++ src/platform.js
@@ -1,7 +1,7 @@
 var imagePlatform=document.getElementById("platform");
 
 class Plateform {
-    constructor({x,y})
+    constructor({x,y}) 
     {
         this.position=
         {
@@ -17,4 +17,23 @@ class Plateform {
         c.drawImage(imagePlatform,this.position.x,this.position.y,this.width,this.height);
     }
 } 
+
+function ajoute(maxX,minX,maxH,minH)
+{
+    xPlatform=Math.floor(Math.random()*(maxX-minY+1)+minX);
+    yPlatform=Math.floor(Math.random()*(maxH-minH+1)+minH);
+    const platform= new Plateform({x:xPlatform,y:yPlatform});
+    platforms.push(platform);
+}
+
+const platforms = [
+    new Plateform({x:256,y: 750})
+    ,new Plateform({x:498,y:520}),
+    new Plateform({x:898, y:656})
+    ,new Plateform({x:1300,y:500})
+    ,new Plateform({x:2100,y:750}),
+    new Plateform({x:2400,y:590})];
+ 
+    
+
 
