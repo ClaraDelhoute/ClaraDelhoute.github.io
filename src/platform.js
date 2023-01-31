eb74252ae7bf57e9cf38a032a9df50c65ae8d108
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2023-01-31 14:08:07 +0100

    Update platforms.js

--- src/platform.js
+++ src/platform.js
@@ -8,7 +8,7 @@ class Plateform {
         x,
         y,
         }
-        this.width=370+Math.floor(Math.random()*(75-26)+26);
+        this.width=370+Math.floor(Math.random()*(200-26)+26);
         this.height=60;
     }
     update()
@@ -23,15 +23,36 @@ class Plateform {
     repop()
     {   if(this.position.x<-150)
         this.position.x=player.position.x+2100;
-    }
+    } 
 } 
 
+var deplace=1;
+dep=-1
+function montePlatform()
+{   
+                platforms[2].position.y -=dep;
+                if(platforms[2].position.y==200)
+                {
+                    dep=-1;
+                   
+                }
+                if(platforms[2].position.y==760)
+                {
+                     dep=1;
+                }
+                
+ }
+
+   
+
+ 
 
 
-const platforms = [
+
+var platforms = [
     new Plateform({x:256,y: 750})
     ,new Plateform({x:498,y:520}),
-    new Plateform({x:898, y:656})
+    new Plateform({x:898, y:656}),
     ,new Plateform({x:1300,y:500})
     ,new Plateform({x:2100,y:750}),
     new Plateform({x:2400,y:590})];
